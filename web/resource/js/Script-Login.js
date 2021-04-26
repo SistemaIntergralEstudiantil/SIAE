3cbd14d083aeb0700c091b529420639ef9b85054
SandraMonserratBL <sbautista@itsoeh.edu.mx>
2021-04-26 12:22:15 -0500

    Diseño del form en Login

--- web/resource/js/Script-Login.js
+++ web/resource/js/Script-Login.js
@@ -45,3 +45,52 @@ function validarPassword( d, regex ){
     
 })(document);
 
+
+
+$(document).ready( 
+    
+    setTimeout(function(){
+    
+    $('.content-form').css("opacity", "1");
+    }, 100)
+)
+
+//Email
+
+$('#user').on('focus', function(){
+    
+    $('.lbl-user').css("margin-top", "-20px");
+    $('.lbl-user').css("font-size", "14px");
+})
+
+$('#user').on('focusout', function(){
+    
+    if($('#user').val().length < 1){
+        
+        $('.lbl-user').css("margin-top", "12px");
+        $('.lbl-user').css("font-size", "20px");
+    }else{
+        $('.lbl-user').css("margin-top", "-20px");
+    $('.lbl-user').css("font-size", "14px");
+    }
+})
+
+//Contraseña
+
+$('#pass').on('focus', function(){
+    
+    $('.lbl-pass').css("margin-top", "10px");
+    $('.lbl-pass').css("font-size", "14px");
+})
+
+$('#pass').on('focusout', function(){
+    
+    if($('#pass').val().length < 1){
+        
+        $('.lbl-pass').css("margin-top", "40px");
+        $('.lbl-pass').css("font-size", "20px");
+    }else{
+        $('.lbl-pass').css("margin-top", "10px");
+    $('.lbl-pass').css("font-size", "14px");
+    }
+})
